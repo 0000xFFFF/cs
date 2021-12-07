2083a24395346a9a8648dd0ca0da1b8aa3992c33
0xC0LD <coldmail@gmail.com>
2021-12-07 20:59:24 +0100

    update

---
+++ utils/resize1920x1080/resize1920x1080/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace resize1920x1080
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Image image = Image.FromFile(arg);
+
+                int width = 1920;
+                int height = 1080;
+                Rectangle destRect = new Rectangle(0, 0, width, height);
+                Bitmap destImage = new Bitmap(width, height);
+
+                destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+                using (var graphics = Graphics.FromImage(destImage))
+                {
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    using (var wrapMode = new ImageAttributes())
+                    {
+                        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    }
+                }
+                image.Dispose();
+                destImage.Save(arg);
+            }
+
+            return 0;
+        }
+    }
+}
